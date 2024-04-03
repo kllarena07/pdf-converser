433f910b92913eaf54ab63d043058a993ba4ab66
Kieran Llarena <baymax709@gmail.com>
2024-04-03 19:33:50 -0400

    feat: parsing pdf as text

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
+import textract, textract.parsers.pdf_parser
+import tempfile
 
 app = FastAPI()
 
 @app.post("/extract/")
 async def extract_text(pdf: UploadFile = File(...)):
-    print(pdf.filename)
-    return {"text": "Hello, World!"}
+    contents = await pdf.read()
+    with tempfile.NamedTemporaryFile(delete=True) as temp:
+        temp.write(contents)
+        temp.flush()
+        context = textract.process(temp.name, encoding='utf-8', extension=".pdf")
+        print(context)
+
+    return {
+        "filename": pdf.filename
+    }
