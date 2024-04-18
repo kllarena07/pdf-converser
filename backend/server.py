2c9a200b0b56fa2dd936e0f924b4e6d5967cefa1
Kieran Llarena <baymax709@gmail.com>
2024-04-17 22:09:46 -0400

    feat(backend): added db resyncing if file wasn't fully uploaded

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,10 @@
 import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, UploadFile, File
-import tempfile
 from pymongo import MongoClient
+from split_pdf import split_pdf
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import MongoDBAtlasVectorSearch
 
 load_dotenv(override=True)
@@ -31,38 +29,46 @@ async def extract_text(pdf: UploadFile = File(...)):
     just_filename = filename[:-4]
     
     collection_to_list = list(collection.find({ "filename": just_filename }))
+    docs = split_pdf(contents, filename)
+    resync=False
     
-    if len(collection_to_list) > 0:
-            return {
-                "filename": just_filename,
-                "message": "Document already exists in the database.",
-                "embeddings_successful": False
-            }
-    
-    with tempfile.NamedTemporaryFile(delete=True) as temp:
-        temp.write(contents)
-        temp.flush()
-
-        loader = PyPDFLoader(temp.name)
-        data = loader.load()
-
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-        docs = text_splitter.split_documents(data)
-        
-        for doc in docs:
-            doc.metadata["filename"] = just_filename
-
-        gemini_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-        
-        MongoDBAtlasVectorSearch.from_documents(
-            documents=docs,
-            embedding=gemini_embeddings,
-            collection=collection,
-            index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME
-        )
-        
+    if len(collection_to_list) == len(docs):
         return {
             "filename": just_filename,
-            "message": "Document successfully extracted, embedded, and inserted into the database.",
-            "embeddings_successful": True
+            "message": "Document already exists in the database.",
+            "embeddings_successful": False
         }
+    
+    if len(collection_to_list) != 0 and len(collection_to_list) != len(docs):
+        collection.delete_many({ "filename": just_filename })
+        print(f"""
+            WARNING: Document lengths do not match.
+            
+            Deleting all documents with the filename: {just_filename} to resync the database.
+        """)
+        resync=True
+    
+    gemini_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    MongoDBAtlasVectorSearch.from_documents(
+        documents=docs,
+        embedding=gemini_embeddings,
+        collection=collection,
+        index_name=ATLAS_VECTOR_SEARCH_INDEX_NAME
+    )
+    
+    return {
+        "filename": just_filename,
+        "message": "Document successfully extracted, embedded, and inserted into the database.",
+        "embeddings_successful": True,
+        "was_resynced": resync
+    }
+
+from pydantic import BaseModel
+
+class UserQuery(BaseModel):
+    query: str
+
+@app.post("/query/")
+async def query_pdf(user_query: UserQuery):
+    return { "query": user_query.query }
