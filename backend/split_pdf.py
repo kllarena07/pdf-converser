89d313bd78059c665413edd470f3f3185bb3accb
Kieran Llarena <baymax709@gmail.com>
2024-04-20 16:19:51 -0400

    chore(backend): edited indentation

--- backend/split_pdf.py
+++ backend/split_pdf.py
@@ -7,16 +7,16 @@ import tempfile
 def split_pdf(contents: bytes, filename: str) -> List[Document]:
     just_filename = filename[:-4]
     with tempfile.NamedTemporaryFile(delete=True) as temp:
-      temp.write(contents)
-      temp.flush()
+        temp.write(contents)
+        temp.flush()
 
-      loader = PyPDFLoader(temp.name)
-      data = loader.load()
+        loader = PyPDFLoader(temp.name)
+        data = loader.load()
 
-      text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-      docs = text_splitter.split_documents(data)
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+        docs = text_splitter.split_documents(data)
       
-      for doc in docs:
-          doc.metadata["filename"] = just_filename
+        for doc in docs:
+            doc.metadata["filename"] = just_filename
       
-      return docs
+        return docs
