0ea4eb8c0c20a7f5c088dcb8635539ad37c2475d
Kieran Llarena <baymax709@gmail.com>
2024-04-20 17:41:40 -0400

    chore(frontend): added conversation type

--- frontend/src/app/_conversation/conversation.tsx
+++ frontend/src/app/_conversation/conversation.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-export default function Conversation() {
+type Conversation = {
+  query: string;
+  result: string;
+}
+
+export default function Conversation({ conversation } : { conversation: Conversation }) {
   return (
     <section className="h-[calc(100dvh-158px)] overflow-y-scroll">
 
