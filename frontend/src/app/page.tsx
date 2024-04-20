b77b5b21729d407c45bdd34e4c2838cdbf6ac671
Kieran Llarena <baymax709@gmail.com>
2024-04-20 17:35:02 -0400

    chore(frontend): init layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Input } from "@/components/ui/input"
+
 export default function Home() {
   const uploadFile = async (formData: FormData) => {
     "use server";
@@ -26,19 +28,25 @@ export default function Home() {
   }
 
   return (
-    <main>
-      <form action={uploadFile}>
-        <label>
-          <input name="pdf" type="file" accept="application/pdf" />
-        </label>
-        <button type="submit">Upload</button>
-      </form>
-      <form action={queryFile}>
-        <label>
-          <input name="query" type="text" placeholder="Message uploaded PDF" />
-        </label>
-        <button type="submit">Search</button>
-      </form>
+    <main className="flex flex-col">
+      <header className="p-5">
+        <form action={uploadFile}>
+          <label>
+            <Input name="pdf" type="file" accept="application/pdf" />
+          </label>
+        </form>
+      </header>
+      <section className="h-[calc(100dvh-158px)] overflow-y-scroll">
+
+      </section>
+      <footer className="w-full flex justify-center">
+        <form action={queryFile} className="pb-5 w-4/5 relative">
+          <label>
+            <Input className="p-7" name="query" type="text" placeholder="Message uploaded PDF" />
+          </label>
+          <button disabled className="absolute disabled:cursor-not-allowed bottom-1.5 right-2 rounded-lg border border-black bg-black p-0.5 text-white transition-colors enabled:bg-black disabled:text-gray-400 disabled:opacity-10 dark:border-white dark:bg-white dark:hover:bg-white md:bottom-3 md:right-3" data-testid="send-button"><span class="" data-state="closed"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" class="text-white dark:text-black"><path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></span></button>
+        </form>
+      </footer>
     </main>
   );
 }
