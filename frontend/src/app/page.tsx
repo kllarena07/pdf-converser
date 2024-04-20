8ec38abff9728200e47b9c1dafbe5cbc30bdad5c
Kieran Llarena <baymax709@gmail.com>
2024-04-20 17:08:49 -0400

    init(frontend): shadcn and input component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,11 +9,35 @@ export default function Home() {
     console.log(await response.json());
   }
 
+  const queryFile = async (formData: FormData) => {
+    "use server";
+
+    const response = await fetch("http://127.0.0.1:8000/query/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        query: formData.get("query"),
+        filename: "lab7"
+      })
+    });
+    console.log(await response.json());
+  }
+
   return (
     <main>
       <form action={uploadFile}>
-        <input name="pdf" type="file" accept="application/pdf" />
+        <label>
+          <input name="pdf" type="file" accept="application/pdf" />
+        </label>
         <button type="submit">Upload</button>
+      </form>
+      <form action={queryFile}>
+        <label>
+          <input name="query" type="text" placeholder="Message uploaded PDF" />
+        </label>
+        <button type="submit">Search</button>
       </form>
     </main>
   );
